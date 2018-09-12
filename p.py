a039aea6589ab49bdbe8139a296263fee8f22763
Sebastian Rouse 19 <SebastianRouse19@newmanschool.org>
2018-09-11 22:25:48 -0500

    started on the clock class by using datetime

--- p.py
+++ p.py
@@ -1,1 +1,15 @@
-p = 2
+import datetime
+import pygame
+now = datetime.datetime.now()
+
+def main():
+    pygame.init()
+    pygame.display.init()
+
+class Clock():
+    def __init__(self):
+        print("hi")
+    def displayTime():
+        pygame.font.Font.render(datetime.datetime.now())
+    def displayDate():
+        pygame.font.Font.render(str(now.day) + "-" + str(now.month) + "-" + str(now.year))
